7d1d5fce5b76a2343f6ca58150ae02a2211e029c
Rodolfo <rodolfougaldeochoa@gmail.com>
2024-02-28 21:58:46 -0600

    :tada: Se inicia ...
    Se inicia proyecto con una structura base

---
+++ apps/static/js/myjs.js
@@ -0,0 +1,29 @@
+// Initialize js files
+
+
+// import { animate } from "motion"
+
+console.log("Hello, World!")
+
+// const $targetEl = document.getElementById('toast-danger');
+
+
+// function hideToast() {
+//   console.log("Se ejecutó el setTimeout 5 segundos.");
+//   $targetEl.classList.add('duration-200');
+//   $targetEl.classList.add('ease-in');
+//   $targetEl.classList.add('opacity-100');
+//   $targetEl.classList.add('translate-y-0');
+//   $targetEl.classList.add('opacity-0');
+//   $targetEl.classList.add('translate-y-5');
+// }
+//
+// setTimeout(() => {
+//     hideToast();
+// }, 5000 );
+
+
+// console.log($targetEl)
+
+
+
