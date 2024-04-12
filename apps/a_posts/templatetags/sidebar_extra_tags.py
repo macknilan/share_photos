a0af0f8d73f23a03db5e67a6a426ec0c13f8f370
Rodolfo <rodolfougaldeochoa@gmail.com>
2024-04-11 18:18:04 -0600

    :sparkles: [ADD] Sort top posts and tops comments ...
    Se agrega nueva funcionalidad en -sidebar.html- para mostrar -Top post- y -Top Comment- para mostrar cuales son lo que cuentan con mas comentarios(5) y posts

--- apps/a_posts/templatetags/sidebar_extra_tags.py
+++ apps/a_posts/templatetags/sidebar_extra_tags.py
@@ -1,21 +1,27 @@
+from django.db.models import Count
 from django.template import Library
 
-from apps.a_posts.models import Tag
+from apps.a_posts.models import Tag, Post, Comment
 
 
 register = Library()
 
 
 @register.inclusion_tag("includes/sidebar.html")
-def sidebar_extra_tag_view():
+def sidebar_extra_tag_view(tag=None, user=None):
     """
     RETURN CUSTOM TEMPLATE TAG TO INCLUDE IN THE SIDEBAR
     """
-
     categories = Tag.objects.all()
+    top_posts = Post.objects.annotate(num_likes=Count("likes")).filter(num_likes__gt=0).order_by("-num_likes")[:5]
+    top_comments = Comment.objects.annotate(num_likes=Count("likes")).filter(num_likes__gt=0).order_by("-num_likes")[:5]
 
     context = {
         "categories": categories,
+        "tag": tag,
+        "top_comments": top_comments,
+        "top_posts": top_posts,
+        "user": user,
     }
 
     return context
